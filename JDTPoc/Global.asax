76c7e6426385ab38f5f2205b8764f540f1f7eb9f
crusn1k <crusnik02.trinityblood@gmail.com>
2014-09-02 15:47:57 +0530

    Created MVC based project

---
+++ JDTPoc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JDTPoc.MvcApplication" Language="C#" %>
---
+++ JDTPoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JDTPoc.Startup))]
+namespace JDTPoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
